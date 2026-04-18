d7123cda76c09a08e5439d07e139c57161002946
bhare8249-wq <bhare8249@gmail.com>
2026-04-18 18:37:26 -0400

    Add Capacitor with iOS and Android platforms

---
+++ android/app/src/main/java/com/barbellabs/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.barbellabs.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.barbellabs.app',
+  appName: 'Barbell Labs',
+  webDir: 'build'
+};
+
+export default config;
